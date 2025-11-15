5180b3474501e953e602aa54488d05f4b6d87e00
Samarth MAlhotra <samarthmalhotra4200@gmail.com>
2025-11-15 15:22:14 +0530

    add changes

--- src/Components/PieChartInFlexbox.jsx
+++ src/Components/PieChartInFlexbox.jsx
@@ -7,7 +7,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import "./skill.css";
+import "./Skill.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import data from "./Chart.js";
